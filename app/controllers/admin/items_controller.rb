e6ca371c8b9cb3ccc6b37c0e500a9a7d12a12027
siydec <97608879+siydec@users.noreply.github.com>
2022-03-05 09:55:24 +0000

    update

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,14 +1,22 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items = Item.page(params[:page])
   end
 
   def new
+    @item = Item.new
+    @genre = Genre.all
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to action: "index"
   end
 
   def show
+    @item = Item.find(params[:id])
+
   end
 
   def edit
@@ -16,4 +24,11 @@ class Admin::ItemsController < ApplicationController
 
   def update
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :price, :is_active)
+
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,18 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+  #belongs_to :genre
+
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
+
+  def add_tax_price
+        (self.price * 1.08).round
+  end
+
+  
 end

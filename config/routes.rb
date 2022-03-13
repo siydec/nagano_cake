95eacd8dbd55d254c1aee62e0821eca9ef271371
siydec <97608879+siydec@users.noreply.github.com>
2022-03-13 10:39:58 +0000

    update_public

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,14 @@ Rails.application.routes.draw do
 
 
   namespace :public do
-
-    devise_for :customers, controllers: {
-    registrations: 'public/registrations',
-    sessions: 'public/sessions'
-
-  }
-
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/unsubscribe'
+  end
+  namespace :public do
     root to: "homes#top"
     get "about" => "homes#about"
+    resources :customers
 
 
   end
@@ -26,6 +25,12 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get "home/about" => "homes#about", as: "about"
   devise_for :admins
+  devise_for :customers, controllers: {
+    registrations: 'public/registrations',
+    sessions: 'public/sessions'
+
+  }
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :genres

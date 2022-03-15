25b8e263c170bbc4b3c32613aec1c242f4fbe4f9
siydec <97608879+siydec@users.noreply.github.com>
2022-03-15 13:19:17 +0000

    update

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -19,13 +19,13 @@ class Public::CustomersController < ApplicationController
 
   def withdraw
     @customer = Customer.find(current_customer[:id])
-    @customer.update
+    @customer.update(customer_params)
   end
 
   private
 
   def customer_params
-    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :password, :password_confirmation )
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :password, :password_confirmation, :is_active )
   end
 
 end

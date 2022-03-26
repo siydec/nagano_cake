e8117cd4c57e88353c2031c4a2fc80d60243a485
siydec <97608879+siydec@users.noreply.github.com>
2022-03-26 02:17:12 +0000

    update_public_cart

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -10,14 +10,26 @@ class Public::OrdersController < ApplicationController
 
   def confirm
     @order = Order.new(order_params)
-   binding.pry
+    @customer = Customer.find(current_customer.id)
+    @cart_items = current_customer.cart_items
+    if params[:order][:select_address] == "0"
+      @order.delivery_address = @customer.address
+      @order.delivery_postal_code = @customer.postal_code
+      @order.delivery_name = @customer.last_name + @customer.first_name
+    elsif params[:order][:select_adress] == "1"
+      @address = Address.find(params[:order][:address_id])
+      @order.delivery_address = @address.addresses
+      @order.delivery_postal_code = @address.postalcode
+      @order.delivery_name = @address.name
+    else
+      redirect_to "/public"
+    end
 
   end
 
   def complete
   end
 
-  def
 
   def create
   end
@@ -32,6 +44,6 @@ class Public::OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:payment)
+    params.permit(:payment, :delivery_address, :delivery_postal_code, :delivery_name, :select_address)
   end
 end

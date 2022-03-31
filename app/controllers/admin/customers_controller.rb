c880d9ef55eead22275ce243c5f961bf94bbe751
siydec <hmmtsiy.dec.tf@gmail.com>
2022-03-31 14:10:24 +0000

    fix

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -20,7 +20,7 @@ class Admin::CustomersController < ApplicationController
   private
 
 def customer_params
-    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :password, :password_confirmation )
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :password, :password_confirmation, :is_active )
 end
 
 
